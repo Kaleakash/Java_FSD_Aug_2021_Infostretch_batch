8cfde4bc316ea4b93a1769bb2bd598dcf5254e3b
akash <akash300383@gmail.com>
2021-08-18 12:55:54 +0530

    done

--- Java Programs/Test.java
+++ Java Programs/Test.java
@@ -1,34 +1,27 @@
-class Employee {
-	int id;
-	String name;
-	float salary;
-	Employee() {
-		this.id = 123;
-		this.name = "Unknown";
-		this.salary = 8000;
+class A implements Runnable{
+	@Override
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("i "+i);
+		}
 	}
-	Employee(int id) {
-		this();
-		this.id = id;
-	}
-	Employee(int id, String name) {
-		this(id);
-		this.name = name;
-	}
-	void dis() {
-		System.out.println("Id is "+id);
-		System.out.println("Name is "+name);
-		System.out.println("Salary is "+salary);
-	}	
 }
+class B implements Runnable{
+	@Override
+	public void run() {
+		for(int j=0;j<10;j++) {
+			System.out.println("j "+j);
+		}
+	}
 
+}
 class Test {
-	public static void main(String args[]) {
-	Employee emp1 = new Employee();		emp1.dis();
-	Employee emp2 = new Employee(100);		emp2.dis();
-	Employee emp3 = new Employee(101,"Raju");	emp3.dis();
-	//Employee emp4 = new Employee(102,"Ramesh",14000);
-
-	
+	public static void main(String args[]) throws Exception {
+	Runnable r1 = new A();
+	Runnable r2 = new B();
+	Thread t1 = new Thread(r1);
+	Thread t2 = new Thread(r2);
+	t1.start();
+	t2.start();		
 	}
-}
+}	
